41cc69b4d36ff6fdf0b9a64e9bebcbac0e093235
Aaron Balson <aaronbcj@yahoo.co.in>
2016-03-13 03:29:14 +0530

    soln structure inital

---
+++ LCFdnxcore50/src/aaronbcj.application.service/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("aaronbcj.application.service")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("aaronbcj.application.service")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3ffc99e6-f164-4d37-b698-541b1dd94ef2")]
--- LCFdnxcore50/src/aaronbcj.infra.data/Properties/AssemblyInfo.cs
+++ LCFdnxcore50/src/aaronbcj.infra.data/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("shared.kernel")]
+[assembly: AssemblyTitle("aaronbcj.infra.data")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("shared.kernel")]
+[assembly: AssemblyProduct("aaronbcj.infra.data")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,4 +20,4 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e0f03261-32be-42a9-8761-3782bae37048")]
+[assembly: Guid("f323c5b5-e17e-4faa-88bd-27a81ef0f481")]
